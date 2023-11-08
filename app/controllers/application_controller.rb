c712d3dca3e9ec53664f8d4ecfd891f367bb912d
fukkusu <janette.fuchs@gtmi.co.jp>
2023-11-08 13:33:25 +0900

    README changes
    Learning about how to make (use of) branches.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "¡Hola, Mundo!"
     end
+    def goodbye
+        render html: "Goodbye, World!"
+    end
 end
